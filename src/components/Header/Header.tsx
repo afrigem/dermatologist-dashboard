79ec6f694e418e46c5818a03bf5177508d561360
gersh-codes <gershomochanda@gmail.com>
2024-11-12 20:30:20 +0300

    Login components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import {Avatar} from "@mui/material";
+import { useSession} from "next-auth/react";
 
-const Dashboard = () => {
+const Header = () => {
+    const { data: session } = useSession();
+    const userProfileImg = session?.user?.image as string;
     return (
-        <h1>Header</h1>
+        <>
+            <h1>Header</h1>
+            <Avatar alt={session?.user?.name as string} src={userProfileImg} />        
+        </>
+        
     )
 }
 
-export default Dashboard;
+export default Header;
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,14 @@
+import { Avatar } from "@mui/material"
 import { useSession, signIn, signOut } from "next-auth/react"
 
 const Login = () => {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
+  const userProfileImg = session?.user?.image as string;
+
   if(session) {
     return <>
       Signed in as {session?.user?.email} <br/>
-      <p>Welcome {session?.user?.email}</p>
+      <p>Welcome {session?.user?.name}</p>
       <button onClick={() => signOut()}>Sign out</button>
     </>
   }
